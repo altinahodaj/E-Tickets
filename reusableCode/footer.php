11d5c7b96ed6a9e30a6dfaf161a99344e7518912
unknown <endrit.makolli7@gmail.com>
2022-02-27 13:04:47 +0100

    reusable and responsive footer

---
+++ reusableCode/footer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/footer.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+        <footer>
+            <div class="footer">
+                <hr>
+                <div class=firstfooter-div>
+                    <ul>
+                        <li><strong>E - Tickets</strong><br> Our Logo</li>
+                        <li>E-Tickets</li>
+                        <li>Our Community</li>
+                        <li>Our Company</li>
+                        <li>© E-Tickets, Inc. 2021. <br> We love our users!</li>
+                    </ul>
+                    <ul>
+                        <li><a href="https://www.facebook.com/"><img src="../media/facebook.png"></a></li>
+                        <li><a href="https://twitter.com/home"><img src="../media/twitter.png" ></a></li>
+                        <li><a href="https://www.instagram.com/"><img src="../media/instagram.png"></a></li>
+                        <li><a href="https://www.reddit.com/"><img src="../media/reddit.png"></a></li>
+                    </ul>
+                </div>       
+            </div>
+        </footer>
+</body>
+</html>
